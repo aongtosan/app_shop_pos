8cd207bad43d8ba8a1fb9186f0b3f98339905d17
aongtosan <aongtosan@gmail.com>
2023-09-25 19:55:20 +0700

    init project

---
+++ src/main/java/com/example/app_shop_pos/AppShopPosApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_shop_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppShopPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppShopPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app_shop_pos/AppShopPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_shop_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppShopPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
